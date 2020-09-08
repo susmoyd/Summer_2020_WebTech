74ff87bd1216c370b3f07324c4f0b5b92b4fe10d
Susmoy <susmoyd21@gmail.com>
2020-09-08 23:59:47 +0600

    Final

---
+++ Final_Lab_Exam/user.php
@@ -0,0 +1,79 @@
+<?php
+    session_start();
+?>
+<html>
+    <head>
+        <title>HomePage</title>
+    </head>
+    <body>
+        <table border="1" width=70%>
+            <tr>
+                <td>
+                    User Home Page
+                </td>
+            </tr>
+            
+            <tr>
+                <td align="center">
+                    <h2><p>Welcome User</p></h2>
+                    <div>
+                         <a href="userProfile.html">Profile</a>|
+                         <a href="login.php">Logout</a>
+                    </div>
+                    
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div>
+                        <input type="text" id="search">
+                        <button type="button" onclick="">Search</button>
+                    </div>
+               </td>
+            </tr>
+            
+        </table>
+
+        
+    </body>
+</html><?php
+    session_start();
+?>
+<html>
+    <head>
+        <title>HomePage</title>
+    </head>
+    <body>
+        <table border="1" width=70%>
+            <tr>
+                <td>
+                    User Home Page
+                </td>
+            </tr>
+            
+            <tr>
+                <td align="center">
+                    <h2><p>Welcome User</p></h2>
+                    <div>
+                         <a href="userProfile.html">Profile</a>|
+                         <a href="login.php">Logout</a>
+                    </div>
+                    
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div>
+                        <input type="text" id="search">
+                        <button type="button" onclick="">Search</button>
+                    </div>
+               </td>
+            </tr>
+            
+        </table>
+
+        
+    </body>
+</html>
